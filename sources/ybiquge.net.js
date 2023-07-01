d31e19310875cb59a385913ec7dabc04376b048f
Modificator <yunshangcn@gmail.com>
2023-07-02 01:24:26 +0800

    xcmfu.com.js 添加发现

---
+++ sources/ybiquge.net.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          X小说
+// @domain        xcmfu.com
+// @version       1.0.0
+// @supportURL    https://github.com/open-book-source/booksource-third-party/issues
+// @function      search
+// @function      detail
+// @function      toc
+// @function      chapter
+// ==/UserScript==
